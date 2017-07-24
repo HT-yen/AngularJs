1ed8e466fdd2cec2b4692d6f5ecf50bd9b9d3cca
at-yenho <hothiyen30@gmail.com>
2017-07-25 00:26:53 +0700

    day3

---
+++ src/app/form/app.form.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-root',
+  templateUrl: './app.form.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppFormComponent {
+  show :any;
+  newMem: any;
+  name: string;
+  // newMem: any;
+  // show: any;
+
+  constructor(){
+    
+  }
+
+  addMember(){
+    this.show= document.getElementById("show");
+    this.newMem = document.createElement('li');
+    var span = document.createElement('span');
+    span.innerHTML = this.name;
+    this.newMem.appendChild(span);
+    var del = document.createElement('button');
+    del.innerHTML = `delete`;
+    this.newMem.appendChild(del);
+    this.show.appendChild(this.newMem);
+    del.addEventListener(`click`, (e)=>{
+           var currentNode = e.target;
+           this.deleteButton(currentNode);
+    });
+    }
+    deleteButton(currentNode){
+          var parentDel = currentNode.parentNode;
+           
+          // alert(parentDel);
+          console.log(parentDel);
+           
+          this.show.removeChild(parentDel);
+    }
+}
---
+++ src/app/list/app.list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'list-root',
+  templateUrl: './app.list.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppListComponent {
+  constructor(){
+    
+  }
+}
