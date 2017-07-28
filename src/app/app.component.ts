23a0063daed5e4b8dbfca0cd0b3903f9215a175d
at-yenho <hothiyen30@gmail.com>
2017-07-28 23:34:13 +0700

    day5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  trainers: Array<any>;
+  showmem: any;
+  constructor(){
+  	this.showmem=[];
+  	this.trainers = [{avatar: 'https://i.pinimg.com/236x/e5/d4/57/e5d4574a74b4ed21881f9655543ed078--chibi-fanart.jpg',
+     name: 'HTY', birthday: '10/1/1996', team: 'android'},
+  					{name: 'TKD', birthday: '', team: 'game'}];
+  }
+  showDetail(i){
+    this.showmem = null;
+    setTimeout(()=>{
+      this.showmem= this.trainers[i];
+    });
+  }
 }
